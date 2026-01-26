7240a79b87cfb589ea7fbdc497739bdfff81d9bd
Sylvain Cousineau <sylvain.cousineau@gmail.com>
2026-01-26 13:32:02 -0500

    Update mlxlmprobe to v0.1.2

--- mlxlmprobe.rb
+++ mlxlmprobe.rb
@@ -1,8 +1,8 @@
 class Mlxlmprobe < Formula
   desc "Visual probing and interpretability tool for MLX language models"
   homepage "https://github.com/scouzi1966/MLXLMProbe"
-  url "https://files.pythonhosted.org/packages/37/89/c7244a522292442ea21929a8834768ec078281f7662d7fc408a0fc76a1f9/mlxlmprobe-0.1.1.tar.gz"
-  sha256 "c448d5b3e87682205718119e6c84a029ee4690f3f7c842ab36381f8ac5232d2e"
+  url "https://files.pythonhosted.org/packages/5c/15/9061766150faf70681f9bd6ae0955d7f904147db0bf2c4be908789aabe62/mlxlmprobe-0.1.2.tar.gz"
+  sha256 "aca8737ea8a1e76e466d0f06a99fb30336c04aa4ddc3552d700ec31561e83056"
   license "MIT"
 
   depends_on arch: :arm64
@@ -14,7 +14,7 @@ class Mlxlmprobe < Formula
     system python3, "-m", "venv", libexec
     system libexec/"bin/python", "-m", "ensurepip", "--upgrade"
     system libexec/"bin/pip", "install", "--upgrade", "pip"
-    system libexec/"bin/pip", "install", "mlxlmprobe==0.1.1"
+    system libexec/"bin/pip", "install", "mlxlmprobe==0.1.2"
     (bin/"mlxlmprobe").write_env_script libexec/"bin/mlxlmprobe", PATH: "#{libexec}/bin:$PATH"
   end
 
