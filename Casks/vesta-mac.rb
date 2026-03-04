f1e5547acf238daea3bf2fbc28a1b21d77e3478f
Sylvain Cousineau <sylvain.cousineau@gmail.com>
2026-03-04 09:15:53 -0500

    Update vesta-mac to 0.9.6

--- Casks/vesta-mac.rb
+++ Casks/vesta-mac.rb
@@ -1,6 +1,6 @@
 cask "vesta-mac" do
-  version "0.9.5"
-  sha256 "bf5915425929df1be9c07787d635868a284b7a81e243cdc82d1fe7c8368b376b"
+  version "0.9.6"
+  sha256 "0f4acd25c4b042d57e4e8716e04d3cf993e28bd149c47f7136a3646546cfcf09"
 
   url "https://github.com/scouzi1966/vesta-mac-dist/releases/download/v#{version}/Vesta-#{version}.dmg"
   name "Vesta"
